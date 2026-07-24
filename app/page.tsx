ee26afe80726c7dc9985f536fff186823b7efc80
theonlykurdstan122-debug <theonlykurdstan122@gmail.com>
2026-07-24 21:32:31 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>General AI</h1>
+      <p>Generate AI Images, Videos & Music</p>
+      <button>Get Started</button>
+    </main>
+  );
+}
